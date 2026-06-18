12c45e06e368d18d7e2cbcd4871a1deba34a0862
Nolo Cloud Solutions <thulomaepa@gmail.com>
2026-06-18 14:46:25 +0200

    add create new customer button

--- SADCOMS.Web/src/pages/customers/CustomerListPage.tsx
+++ SADCOMS.Web/src/pages/customers/CustomerListPage.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react'
 import type { Customer, PagedResult } from '../../types'
 import { api } from '../../api/client';
+import { useNavigate } from 'react-router-dom';
 
 export default function CustomerListPage() {
+  const navigate = useNavigate();
+  
   const [customers, setCustomers] = useState<Array<Customer>>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
@@ -18,12 +21,19 @@ export default function CustomerListPage() {
   if (error) return <div>Error: {error}</div>;
   
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh'
+    }}>
       <ul>
         {customers.map(c => (
           <li className='text-white' key={c.id}>{c.name} — {c.email} ({c.countryCode})</li>
         ))}
       </ul>
+      <button onClick={() => navigate('/customers/create')}>Create New Customer</button>
       </div>
   )
 }
