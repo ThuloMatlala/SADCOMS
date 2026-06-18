b7468e11aa41245db0b6e3df788181970d6ced62
Nolo Cloud Solutions <thulomaepa@gmail.com>
2026-06-18 10:28:31 +0200

    add RabbitMQ publisher registration

--- SADCOMS.API/Program.cs
+++ SADCOMS.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SADCOMS.API.Data;
+using SADCOMS.API.Messaging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddSingleton<RabbitMqPublisher>();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
