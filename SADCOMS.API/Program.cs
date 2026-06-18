f1cef253226bb29992a0382acc8d9c9385ec168a
Nolo Cloud Solutions <thulomaepa@gmail.com>
2026-06-18 11:42:37 +0200

    add put and post methods. Enable cors for frontend app on API

--- SADCOMS.API/Program.cs
+++ SADCOMS.API/Program.cs
@@ -10,8 +10,20 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddSingleton<RabbitMqPublisher>();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
 var app = builder.Build();
 
+app.UseCors();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
--- SADCOMS.Web/src/api/client.ts
+++ SADCOMS.Web/src/api/client.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://localhost:5195';
+const BASE_URL = 'http://localhost:5195';
 
 async function request<T>(path: string, options?: RequestInit): Promise<T> { 
   const response = await fetch(`${BASE_URL}${path}`, {
@@ -17,4 +17,12 @@ async function request<T>(path: string, options?: RequestInit): Promise<T> {
 
 export const api = {
   get: <T>(path: string) => request<T>(path),
+  post: <T>(path: string, body: unknown) => request<T>(path, { 
+    method: 'POST', 
+    body: JSON.stringify(body) 
+  }),
+   put: <T>(path: string, body: unknown) => request<T>(path, { 
+    method: 'PUT', 
+    body: JSON.stringify(body) 
+  }),
 }
